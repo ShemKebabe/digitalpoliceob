f127653a7b27633d5dbb9857332092793dd447e6
ShemKebabe <ogendishem1@gmail.com>
2026-09-21 11:56:31 +0300

    add database connection for login

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$host='127.0.0.1';
+$db='digital_police_ob';
+$user='root';
+$pass='';
+$charset = 'utf8mb4';
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+$dsn='mysql:host=$host;dbname=$db;charset=$charset';
+try {
+    $pdo =new PDO($dsn,$user,$pass,$options);
+}
+catch (\PDOException $e) {
+    die("Database connection failed");
+}
+?>
